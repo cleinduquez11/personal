dbee9003c36d3af43b85b0ccb053a88dcca7f17d
cleinduquez11 <cleinduquez11@gmail.com>
2023-11-07 10:58:20 +0800

    updated dev

---
+++ automate.py
@@ -0,0 +1,7 @@
+# for i in range(22):
+#     print('''{"file":''' + '''"thesis/''' + str(i) + '''.jpeg", "isVid": false},''')
+
+
+
+for i in range(24):
+    print('''{"file":''' + '''"dict/''' + str(i) + '''.jpg", "isVid": false},''')
